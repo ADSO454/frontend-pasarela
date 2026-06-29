7053fb925bb759f7f50d51f72214853df4543519
Jeyson David Zuñiga Gomez <jdzunigag@sena.edu.co>
2026-06-28 23:06:00 -0500

    updating project's files

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
-  server: {
-    port: 3000, // Se especifica el puerto para desarrollo
-    open: true,
-    host: true,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_TARGET || 'http://localhost:8080',
-        changeOrigin: true,
-        //secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+    server: {
+      port: 3000,
+      open: true,
+      host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_TARGET || 'http://localhost:8080',
+          changeOrigin: true,
+        },
       },
     },
-  },
-  preview: {
-    port: 8080, // Se especifica el para producción local
-  },
+    preview: {
+      port: 8080,
+    },
+  }
 })
